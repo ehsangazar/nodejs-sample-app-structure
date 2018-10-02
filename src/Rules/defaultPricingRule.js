d1715a1085ff6dfcb1ea44806b05bcc1f2cb6803
Ehsan Gazar <me@ehsangazar.com>
2018-10-03 08:00:41 +1000

    defaultProps data type is edited

--- src/Rules/defaultPricingRule.js
+++ src/Rules/defaultPricingRule.js
@@ -4,67 +4,67 @@ module.exports = {
       name: 'classic',
       standingTime: 'short',
       priority: 0,
-      logo: false,
-      price: 269.99
+      logo: 0,
+      price: 269.99,
     },
     {
       name: 'standout',
       standingTime: 'medium',
       priority: 0,
-      logo: false,
-      price: 322.99
+      logo: 1,
+      price: 322.99,
     },
     {
       name: 'premium',
       standingTime: 'long',
       priority: 0,
-      logo: false,
-      price: 394.99
-    }
+      logo: 1,
+      price: 394.99,
+    },
   ],
   clients: [
-    'default','secondBite', 'axilCoffeeRoasters', 'myer'
+    'base', 'secondBite', 'axilCoffee', 'myer',
   ],
   discounts: [
     {
-      type:'more',
+      type: 'more',
       name: 'moreClassicAds',
       bought: 2,
       adsName: 'classic',
-      willget: 3
+      willget: 3,
     },
     {
       name: 'discountOnStandOut',
-      type:'reduce',
+      type: 'reduce',
       adsName: 'standout',
-      newPrice: 299.99
+      newPrice: 299.99,
     },
     {
-      type:'more',
+      type: 'more',
       name: 'moreOnPremium',
       bought: 4,
       adsName: 'premium',
-      willget: 5
+      willget: 5,
     },
     {
       name: 'discountOnPremium',
-      type:'reduce',
+      type: 'reduce',
       adsName: 'premium',
-      newPrice: 389.99
-    }
+      newPrice: 389.99,
+    },
   ],
   clientDiscount: [
     {
-      name: 'secondBite',
-      discountNames: ['moreClassicAds']
+      clientName: 'secondBite',
+      discountNames: ['moreClassicAds'],
     },
     {
-      name: 'axilCoffeeRoasters',
-      discountNames: ['discountOnStandOut']
+      clientName: 'axilCoffee',
+      discountNames: ['discountOnStandOut'],
     },
     {
-      name: 'myer',
-      discountNames: ['moreOnPremium','discountOnPremium']
-    }
-  ]
-}
+      clientName: 'myer',
+      discountNames: ['moreOnPremium', 'discountOnPremium'],
+    },
+  ],
+};
