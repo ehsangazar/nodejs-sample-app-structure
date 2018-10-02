5c515381461a827fc1303462597d685bae02adba
Ehsan Gazar <me@ehsangazar.com>
2018-10-02 21:22:09 +1000

    change database name for test purposes

--- src/Configs/database.js
+++ src/Configs/database.js
@@ -1,4 +1,14 @@
 const loki = require('lokijs')
-const database = new loki(process.env.DB_NAME || 'example.db')
+let database = undefined
+
+if (process.env.NODE_ENV === 'test'){
+  database = new loki('test.db')
+}
+if (process.env.NODE_ENV === 'development'){
+  database = new loki(process.env.ExampleDB || 'development.db')
+}
+if (process.env.NODE_ENV === 'production'){
+  database = new loki(process.env.ExampleDB || 'production.db')
+}
 
 module.exports = database
